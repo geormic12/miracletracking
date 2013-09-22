9e3157898c4399f80908bf9b2b699d36348f79e9
Mike George <mikegeorge@gmail.com>
2013-09-22 13:01:45 -0500

    Removed Search Bar when users are not logged in

--- wp-content/themes/pinpress/footer.php
+++ wp-content/themes/pinpress/footer.php
@@ -4,7 +4,14 @@
 		<div class="site-info">
 			<a href="http://miracletracking.com/" rel="generator">Proudly powered by Miracles</a>
 		</div><!-- .site-info -->
-		<div  class="searchform_footercover"><?php get_search_form(); ?></div> 
+		<?php	
+		if ( is_user_logged_in()) {
+		?>
+			<div  class="searchform_footercover"><?php get_search_form(); ?></div> 
+		<?php	
+		}
+		?>
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
