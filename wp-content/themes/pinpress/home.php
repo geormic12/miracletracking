4a57853f0f8ff68f283d5431d6256f56ee65b0bb
Mike George <mikegeorge@gmail.com>
2013-09-22 13:02:28 -0500

    Make the home page redirect to login page when users are not logged in

--- wp-content/themes/pinpress/home.php
+++ wp-content/themes/pinpress/home.php
@@ -1,48 +1,55 @@
+<?php	
+if ( !is_user_logged_in()) {
+	wp_redirect( home_url('/login/') );
+	exit;
+}
+?>
+
 <?php get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-<div id="col" style="width:100%;" class="column">
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'content', get_post_format() ); ?>
-			<?php endwhile; ?>
-</div>
-			<?php pin_arc_nav( 'nav-below' ); ?>
+			<div id="col" style="width:100%;" class="column">
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php endwhile; ?>
+			</div>
+				<?php pin_arc_nav( 'nav-below' ); ?>
 
-		<?php else : ?>
+			<?php else : ?>
 
-			<article id="post-0" class="post no-results not-found">
+				<article id="post-0" class="post no-results not-found">
 
-			<?php if ( current_user_can( 'edit_posts' ) ) :
-				// Show a different message to a logged-in user who can add posts.
-			?>
-				<header class="entry-header">
-					<h1 class="entry-title">'No posts to display'</h1>
-				</header>
+				<?php if ( current_user_can( 'edit_posts' ) ) :
+					// Show a different message to a logged-in user who can add posts.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title">'No posts to display'</h1>
+					</header>
 
-				<div class="entry-content">
-					<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'pin' ), admin_url( 'post-new.php' ) ); ?></p>
-				</div><!-- .entry-content -->
+					<div class="entry-content">
+						<p><?php printf( __( 'Ready to publish your first post? <a href="%s">Get started here</a>.', 'pin' ), admin_url( 'post-new.php' ) ); ?></p>
+					</div><!-- .entry-content -->
 
-			<?php else :
-				// Show the default message to everyone else.
-			?>
-				<header class="entry-header">
-					<h1 class="entry-title">'No Miracles matching that search'</h1>
-				</header>
+				<?php else :
+					// Show the default message to everyone else.
+				?>
+					<header class="entry-header">
+						<h1 class="entry-title">'No Miracles matching that search'</h1>
+					</header>
 
-				<div class="entry-content">
-					<p>'No Miracles matching that search'</p>
-					<?php get_search_form(); ?>
-				</div><!-- .entry-content -->
-			<?php endif; // end current_user_can() check ?>
+					<div class="entry-content">
+						<p>'No Miracles matching that search'</p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				<?php endif; // end current_user_can() check ?>
 
-			</article><!-- #post-0 -->
+				</article><!-- #post-0 -->
 
-		<?php endif; // end have_posts() check ?>
+			<?php endif; // end have_posts() check ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
