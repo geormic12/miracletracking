f42989ee98a9a7793c47f4b55b9468cda21625cf
Mike George <mikegeorge@gmail.com>
2013-09-22 00:39:45 -0500

    Updated footer text to be powered by Miracles!

--- wp-content/themes/pinpress/footer.php
+++ wp-content/themes/pinpress/footer.php
@@ -2,7 +2,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
-			<a href="http://wordpress.org/" rel="generator">Proudly powered by WordPress</a>
+			<a href="http://miracletracking.com/" rel="generator">Proudly powered by Miracles</a>
 		</div><!-- .site-info -->
 		<div  class="searchform_footercover"><?php get_search_form(); ?></div> 
 	</footer><!-- #colophon -->
